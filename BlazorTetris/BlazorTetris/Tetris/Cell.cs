000731f13478ad04119190e4aa724208389624a8
Andreas Christensen <andychri@online.no>
2025-07-02 12:49:55 +0200

    Added the board with some minor styling

---
+++ BlazorTetris/BlazorTetris/Tetris/Cell.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace BlazorTetris.Tetris {
+    public class Cell {
+
+        public bool IsFilled { get; set; }
+        public String Color { get; set; } = "#d1cbc7";
+    
+    }
+}
---
+++ BlazorTetris/BlazorTetris/Tetris/Grid.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorTetris.Tetris {
+    public class Grid {
+
+        private readonly int rows = 20;
+        private readonly int columns = 10;
+        public Cell[,] cellGrid = new Cell[20, 10];
+        public Grid() {
+            InitializeGrid();
+        }
+        private void InitializeGrid() {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    cellGrid[row, col] = new Cell();
+                }
+            }
+        }
+
+        public Cell this[int row, int col] {
+            get { return cellGrid[row, col]; }
+        }
+    }
+}
